85e31bb1f892a5b96e295c5275628f781c15cf42
MengZhang <meng.zhang15@ufl.edu>
2013-04-15 12:18:45 -0400

    Support new AUTO_PDATE in field overlay

--- src/main/java/org/agmip/translators/csv/DomeInput.java
+++ src/main/java/org/agmip/translators/csv/DomeInput.java
@@ -16,16 +16,19 @@ import org.slf4j.LoggerFactory;
 import org.agmip.core.types.TranslatorInput;
 
 public class DomeInput implements TranslatorInput {
+
     private static final Logger log = LoggerFactory.getLogger(DomeInput.class);
     private HashMap<String, Object> dome = new HashMap<String, Object>();
     private ArrayList<String> generatorFunctions = new ArrayList<String>();
-
+    private ArrayList<String> ruleFunctions = new ArrayList<String>();
 
     public DomeInput() {
         generatorFunctions.add("AUTO_PDATE()");
         generatorFunctions.add("AUTO_REPLICATE_EVENTS()");
+        ruleFunctions.add("AUTO_PDATE()");
     }
 
+    @Override
     public Map readFile(String fileName) throws Exception {
         if (fileName.toUpperCase().endsWith("CSV")) {
             FileInputStream stream = new FileInputStream(fileName);
@@ -37,7 +40,9 @@ public class DomeInput implements TranslatorInput {
 
     public void readCSV(InputStream stream) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-        HashMap<String, String> info  = new HashMap<String, String>();
+        HashMap<String, String> info = new HashMap<String, String>();
+//        HashMap<String, ArrayList<String>> linkOvl = new HashMap<String, ArrayList<String>>();
+//        HashMap<String, ArrayList<String>> linkStg = new HashMap<String, ArrayList<String>>();
         ArrayList<HashMap<String, String>> rules = new ArrayList<HashMap<String, String>>();
         ArrayList<HashMap<String, String>> generators = new ArrayList<HashMap<String, String>>();
         CSVReader reader = new CSVReader(br);
@@ -61,18 +66,20 @@ public class DomeInput implements TranslatorInput {
                         log.debug("Found fun {}", nextLine[3].toUpperCase());
                         // Handle Arguments
                         if (generatorFunctions.contains(nextLine[3].toUpperCase())) {
-                            log.debug("Found a generator!!!");
-                            isGenerator = true;
-                            hasGenerator = true;
+                            if (!cmd.equals("FILL") || !ruleFunctions.contains(nextLine[3].toUpperCase())) {
+                                log.debug("Found a generator!!!");
+                                isGenerator = true;
+                                hasGenerator = true;
+                            }
                         }
                         int argLen = nextLine.length - 3;
 
                         if (argLen != 0) {
                             args.append(nextLine[3]);
                             if (argLen > 1) {
-                                for( int i = 4; i < nextLine.length; i++) {
-                                    args.append("|");  
-                                    if (! nextLine[i].startsWith("!")) {
+                                for (int i = 4; i < nextLine.length; i++) {
+                                    args.append("|");
+                                    if (!nextLine[i].startsWith("!")) {
                                         args.append(nextLine[i].toUpperCase());
                                     }
                                 }
@@ -84,10 +91,10 @@ public class DomeInput implements TranslatorInput {
                         if (chopIndex == -1) {
                             chopIndex = args.length();
                         }
-                        if (args.substring(0,chopIndex).endsWith("|")) {
-                            chopIndex --;
+                        if (args.substring(0, chopIndex).endsWith("|")) {
+                            chopIndex--;
                         }
-                        lineMap.put("args", args.substring(0,chopIndex));
+                        lineMap.put("args", args.substring(0, chopIndex));
                     } else {
                         // Variable or static
                         lineMap.put("args", nextLine[3].toUpperCase());
@@ -100,20 +107,52 @@ public class DomeInput implements TranslatorInput {
                     } else {
                         rules.add(lineMap);
                     }
+//                } else if (cmd.equals("LINK")) {
+//                    String keyType = nextLine[2].trim().toUpperCase();
+//                    String keyValue = nextLine[3].trim();
+//                    String key = keyType + "_" + keyValue;
+//                    String domeType = nextLine[4].trim();
+//                    ArrayList<String> domeIds;
+//                    if (domeType.toUpperCase().equals("OVERLAY")) {
+//                        domeIds = getDomeIds(linkOvl, key);
+//                    } else if (domeType.toUpperCase().equals("STRATEGY")) {
+//                        domeIds = getDomeIds(linkStg, key);
+//                    } else {
+//                        log.error("Found invalid DOME type {} at line {}", domeType, ln);
+//                        continue;
+//                    }
+//                    for (int i = 5; i < nextLine.length; i++) {
+//                        if (nextLine[i].trim().startsWith("!")) {
+//                            break;
+//                        } else if (!nextLine[i].trim().equals("")) {
+//                            domeIds.add(nextLine[i].trim());
+//                        }
+//                    }
                 } else {
                     log.error("Found invalid command {} at line {}", cmd, ln);
                 }
-            } 
+            }
         }
         br.close();
-        if (hasGenerator) {               
+        if (hasGenerator) {
             dome.put("generators", generators);
         }
         dome.put("info", info);
         dome.put("rules", rules);
+//        dome.put("link_overlay", linkOvl);
+//        dome.put("link_stragty", linkStg);
     }
 
     public HashMap<String, Object> getDome() {
         return dome;
     }
+
+//    private ArrayList<String> getDomeIds(HashMap<String, ArrayList<String>> link, String key) {
+//        ArrayList<String> domeIds = link.get(key);
+//        if (domeIds == null) {
+//            domeIds = new ArrayList<String>();
+//            link.put(key, domeIds);
+//        }
+//        return domeIds;
+//    }
 }
