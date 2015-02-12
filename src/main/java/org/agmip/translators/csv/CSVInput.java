5c4081e4754715cc63ddf9add26a08ba56741b88
MengZhang <meng.zhang15@ufl.edu>
2015-02-12 02:00:03 -0500

    Add handling to support read AgTrails data CSV files

--- src/main/java/org/agmip/translators/csv/CSVInput.java
+++ src/main/java/org/agmip/translators/csv/CSVInput.java
@@ -16,13 +16,16 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipEntry;
 
 import au.com.bytecode.opencsv.CSVReader;
+import java.io.IOException;
 import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.agmip.ace.AcePathfinder;
+import org.agmip.ace.LookupCodes;
 import org.agmip.ace.util.AcePathfinderUtil;
+import org.agmip.common.Functions;
 import org.agmip.core.types.TranslatorInput;
 
 /**
@@ -50,12 +53,16 @@ public class CSVInput implements TranslatorInput {
     private String listSeparator;
     private AcePathfinder pathfinder = AcePathfinderUtil.getInstance();
     private static HashSet unknowVars = new HashSet();
+    private boolean isAgTrails = false;
+    private boolean isAgTrailsExist = false;
 
     private enum HeaderType {
 
         UNKNOWN, // Probably uninitialized
         SUMMARY, // #
-        SERIES   // %
+        SERIES,   // %
+        AT_META,
+        AT_DATA
     }
 
     private static class CSVHeader {
@@ -64,6 +71,9 @@ public class CSVInput implements TranslatorInput {
         private final ArrayList<Integer> skippedColumns;
         private final String defPath;
         private final AcePathfinderUtil.PathType defPathType;
+        private HashMap<String, String> agtrMapping;
+        private int agrtVarCol = -1;
+        private int agrtValCol = -1;
 
         public CSVHeader(ArrayList<String> headers, ArrayList<Integer> sc) {
             this(headers, sc, null, AcePathfinderUtil.PathType.UNKNOWN);
@@ -74,6 +84,24 @@ public class CSVInput implements TranslatorInput {
             this.skippedColumns = sc;
             this.defPath = defPath;
             this.defPathType = defPathType;
+        }
+        
+        public CSVHeader(ArrayList<String> headers, ArrayList<Integer> sc, String defPath, AcePathfinderUtil.PathType defPathType, HashMap<String, String> mapping) {
+            this(headers, sc, defPath, defPathType);
+            this.agtrMapping = mapping;
+            for (int i = 0; i < headers.size(); i++) {
+                if (headers.get(i).equals("variables measured")) {
+                    this.agrtVarCol = i + 1;
+                } else if (headers.get(i).equals("value")) {
+                    this.agrtValCol = i + 1;
+                }
+            }
+            if (this.agrtVarCol < 0) {
+                this.agrtVarCol = 2;
+            }
+            if (this.agrtValCol < 0) {
+                this.agrtValCol = 3;
+            }
         }
 
         public CSVHeader() {
@@ -97,6 +125,18 @@ public class CSVInput implements TranslatorInput {
 
         public AcePathfinderUtil.PathType getDefPathType() {
             return defPathType;
+        }
+
+        public HashMap<String, String> getAgtrMapping() {
+            return this.agtrMapping;
+        }
+
+        public int getAgtrVarCol() {
+            return this.agrtVarCol;
+        }
+
+        public int getAgtrValCol() {
+            return this.agrtValCol;
         }
     }
 
@@ -143,8 +183,13 @@ public class CSVInput implements TranslatorInput {
         BufferedReader br = new BufferedReader(new InputStreamReader(fileStream));
 
         // Check to see if this is an international CSV. (;, vs ,.)
+        this.isAgTrails = false;
+        HashMap<String, String> mapping = null;
         setListSeparator(br);
         CSVReader reader = new CSVReader(br, this.listSeparator.charAt(0));
+        if (this.isAgTrails) {
+            mapping = readAgTrail2AgMipMap();
+        }
 
         // Clear out the idMap for every file created.
         idMap.clear();
@@ -153,7 +198,18 @@ public class CSVInput implements TranslatorInput {
         while ((nextLine = reader.readNext()) != null) {
             ln++;
             LOG.debug("Line number: " + ln);
-            if (nextLine[0].startsWith("!")) {
+            if (isAgTrails && nextLine[0].equals("Replication")) {
+                LOG.debug("Found an AgTrails meta header line");
+                section = HeaderType.AT_DATA;
+                currentHeader = parseHeaderLineForAgTrails(nextLine, mapping);
+            } else if (isAgTrails && nextLine[0].equals("Id Trials")) {
+                LOG.debug("Found an AgTrails data header line");
+                section = HeaderType.AT_META;
+                currentHeader = parseHeaderLineForAgTrails(nextLine, mapping);
+            } else if (isAgTrails) {
+                LOG.debug("Found an AgTrails line");
+                parseDataLine(currentHeader, section, nextLine, true);
+            } else if (nextLine[0].startsWith("!")) {
                 LOG.debug("Found a comment line");
                 continue;
             } else if (nextLine[0].startsWith("#")) {
@@ -218,11 +274,87 @@ public class CSVInput implements TranslatorInput {
         return new CSVHeader(h, sc, defPath, defPathType);
     }
 
+    protected CSVHeader parseHeaderLineForAgTrails(String[] data, HashMap<String, String> mapping) {
+        ArrayList<String> h = new ArrayList<String>();
+        ArrayList<Integer> sc = new ArrayList<Integer>();
+        String defPath = null;
+        AcePathfinderUtil.PathType defPathType = AcePathfinderUtil.PathType.UNKNOWN;
+
+        int l = data.length;
+        for (int i = 1; i < l; i++) {
+            String var = mapping.get(data[i].toLowerCase());
+            if (var == null) {
+                sc.add(i);
+                var = data[i].trim().toLowerCase();
+            }
+            if (var.length() != 0) {
+                h.add(var);
+            }
+            if (defPath == null) {
+                defPath = AcePathfinderUtil.getInstance().getPath(var);
+                if (defPath != null) {
+                    defPath = defPath.replaceAll(",", "").trim();
+                    defPathType = AcePathfinderUtil.getVariableType(var);
+                } else if (data[0].equals("Replication")) {
+                    defPathType = AcePathfinderUtil.PathType.EXPERIMENT;
+                }
+            }
+        }
+        return new CSVHeader(h, sc, defPath, defPathType, mapping);
+    }
+
+    protected HashMap<String, String> readAgTrail2AgMipMap() {
+        HashMap<String, String> mapping = new HashMap();
+        InputStream res = getClass().getClassLoader().getResourceAsStream("AgTrials2AgMIPVarMap.csv");
+        try {
+            if( res != null ) {
+                CSVReader reader = new CSVReader(new InputStreamReader(res));
+                // Get AgTrails and AgMaps column number
+                String[] line = reader.readNext();
+                int agtrailsCol = 1;
+                int agmipCol = 2;
+                for (int i = 0; i < line.length; i++) {
+                    if (line[i].startsWith("AgTrials")) {
+                        agtrailsCol = i;
+                    } else if (line[i].startsWith("ICASA")) {
+                        agmipCol = i;
+                    }
+                }
+                // Read mapping definition
+                while(( line = reader.readNext()) != null) {
+                    String agtrailsVar = line[agtrailsCol].toLowerCase();
+                    String agmipVar = line[agmipCol].toLowerCase();
+                    if (agtrailsVar.contains(";")) {
+                        String[] agtrailsVars = agtrailsVar.split(";");
+                        for (String var : agtrailsVars) {
+                            mapping.put(var, agmipVar);
+                        }
+                    } else {
+                        mapping.put(agtrailsVar, agmipVar);
+                    }
+                }
+                reader.close();
+            } else {
+                LOG.error("Missing embedded CSV file for configuration. no AgTrails variable will be recognized");
+            }
+        } catch(IOException ex) {
+            LOG.debug(ex.toString());
+            throw new RuntimeException(ex);
+        }
+
+        return mapping;
+    }
+
     protected void parseDataLine(CSVHeader header, HeaderType section, String[] data, boolean isComplete) throws Exception {
         ArrayList<String> headers = header.getHeaders();
         int l = headers.size();
         String dataIndex;
         dataIndex = UUID.randomUUID().toString();
+        if (section.equals(HeaderType.AT_DATA)) {
+            isComplete = false;
+        } else if (section.equals(HeaderType.AT_META)) {
+            dataIndex = "AgTrails_" + dataIndex;
+        }
 
         if (!isComplete) {
             if (idMap.containsKey(data[0])) {
@@ -231,7 +363,18 @@ public class CSVInput implements TranslatorInput {
                 idMap.put(data[0], dataIndex);
             }
         }
-        if (data[1].toLowerCase().equals("event")) {
+        if (section.equals(HeaderType.AT_DATA)) {
+            int varCol = header.getAgtrVarCol();
+            int valCol = header.getAgtrValCol();
+            String var = header.getAgtrMapping().get(data[varCol]);
+            String val = converAgtrData(var, data[valCol]);
+            if (var == null) {
+                LOG.warn("{} is not defined in the mapping", data[varCol]);
+            } else {
+                insertValue(dataIndex, var, val, header);
+            }
+            
+        } else if (data[1].toLowerCase().equals("event")) {
             for (int i = 3; i < data.length; i++) {
                 String var = data[i].toLowerCase();
                 i++;
@@ -248,7 +391,11 @@ public class CSVInput implements TranslatorInput {
         } else if (header.getSkippedColumns().isEmpty()) {
             for (int i = 0; i < l; i++) {
                 if (!data[i + 1].equals("")) {
-                    insertValue(dataIndex, headers.get(i), data[i + 1], header);
+                    if (section.equals(HeaderType.AT_META)) {
+                        insertValue(dataIndex, headers.get(i), converAgtrMeta(headers.get(i), data[i + 1]), header);
+                    } else {
+                        insertValue(dataIndex, headers.get(i), data[i + 1], header);
+                    }
                 }
             }
         } else {
@@ -256,7 +403,11 @@ public class CSVInput implements TranslatorInput {
             for (int i = 0; i < l; i++) {
                 if (!data[i + 1].equals("")) {
                     if (!skipped.contains(i + 1)) {
-                        insertValue(dataIndex, headers.get(i), data[i + 1], header);
+                        if (section.equals(HeaderType.AT_META)) {
+                            insertValue(dataIndex, headers.get(i), converAgtrMeta(headers.get(i), data[i + 1]), header);
+                        } else {
+                            insertValue(dataIndex, headers.get(i), data[i + 1], header);
+                        }
                     }
                 }
             }
@@ -342,16 +493,72 @@ public class CSVInput implements TranslatorInput {
 
         }
     }
+    
+    private String converAgtrMeta(String var, String val) {
+        if (var.equals("pdate") || var.equals("hdate")) { // TODO
+            if (val.length() == 5) {
+                try {
+                    long date = (Long.parseLong(val) - 25568l) * 86400000l;
+                    return Functions.convertToAgmipDateString(new Date(date));
+                } catch (Exception e) {
+                    LOG.warn("Invalid number of days since 1970/01/01 for [{}]: {}", var, val);
+                    LOG.warn(Functions.getStackTrace(e));
+                    return val;
+                }
+            } else if (val.matches("\\d{4}[\\\\\\-/]\\d{2}[\\\\\\-/]\\d{2}")) {
+                return val.replaceAll("[\\\\\\-/]{1}", "");
+            } else {
+                return val;
+            }
+        } else if (var.equals("crid")) {
+            return LookupCodes.lookupCode("CRID", val, "code", "APSIM").toUpperCase(); // TODO
+        } else {
+            return val;
+        }
+    }
+    
+    private String converAgtrData(String var, String val) {
+        if (var.equals("hwah") || var.equals("chtx")) {
+            return Functions.divide(val, "100");
+        } else if (var.equals("hwah")) {
+            return Functions.product(val, "1000", "0.875");
+        } else if (var.equals("plpop")) {
+            return ""; // TODO
+        } else if (var.equals("plrs")) {
+            return ""; // TODO
+        } else if (var.equals("hwah")) {
+            return "";
+        } else {
+            return val;
+        }
+    }
 
     protected HashMap<String, ArrayList<HashMap<String, Object>>> cleanUpFinalMap() {
         HashMap<String, ArrayList<HashMap<String, Object>>> base = new HashMap<String, ArrayList<HashMap<String, Object>>>();
         ArrayList<HashMap<String, Object>> experiments = new ArrayList<HashMap<String, Object>>();
         ArrayList<HashMap<String, Object>> weathers = new ArrayList<HashMap<String, Object>>();
         ArrayList<HashMap<String, Object>> soils = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> metaMap = null;
 
+        if (this.isAgTrailsExist) {
+            for (int i = 0; i < orderring.size(); i++) {
+                if (orderring.get(i).startsWith("AgTrails_")) {
+                    String metaId = orderring.remove(i);
+                    metaMap = expMap.remove(metaId);
+                    break;
+                }
+            }
+            metaMap.put("data_source", "AgTrails");
+        }
+        
         for (String id : orderring) {
             //for (HashMap<String, Object> ex : expMap.values()) {
-            HashMap<String, Object> ex = expMap.get(id);
+            HashMap<String, Object> ex;
+            if (this.isAgTrailsExist) {
+                ex = combineMap(metaMap, expMap.get(id));
+            } else {
+                ex = expMap.get(id);
+            }
             ex.remove("weather");
             ex.remove("soil");
             if (ex.size() == 2 && ex.containsKey("wst_id") && ex.containsKey("soil_id")) {
@@ -396,6 +603,27 @@ public class CSVInput implements TranslatorInput {
         base.put("soils", soils);
         return base;
     }
+    
+    private HashMap<String, Object> combineMap(HashMap<String, Object> base, HashMap<String, Object> in) {
+        HashMap<String, Object> ret = new HashMap(base);
+        for (Map.Entry<String, Object> entry : in.entrySet()) {
+            Object orgVal = ret.get(entry.getKey());
+            if (orgVal == null || entry.getValue() instanceof String) {
+                ret.put(entry.getKey(), entry.getValue());
+            } else if (entry.getValue() instanceof ArrayList) {
+                if (ret.get(entry.getKey()) instanceof ArrayList) {
+                    ((ArrayList) orgVal).addAll((ArrayList) entry.getValue());
+                } else {
+                    ret.put(entry.getKey(), entry.getValue());
+                }
+            } else if (entry.getValue() instanceof HashMap) {
+                combineMap((HashMap<String, Object>) orgVal, (HashMap<String, Object>) entry.getValue());
+            } else {
+                ret.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return ret;
+    }
 
     protected void setListSeparator(BufferedReader in) throws Exception {
         // Set a mark at the beginning of the file, so we can get back to it.
@@ -406,6 +634,19 @@ public class CSVInput implements TranslatorInput {
                 String listSeperator = sample.substring(1, 2);
                 LOG.debug("FOUND SEPARATOR: " + listSeperator);
                 this.listSeparator = listSeperator;
+                break;
+            } else if (sample.startsWith("Replication")) {
+                String listSeperator = sample.substring(11, 12);
+                LOG.debug("FOUND SEPARATOR: " + listSeperator);
+                this.listSeparator = listSeperator;
+                this.isAgTrails = true;
+                break;
+            } else if (sample.startsWith("Id Trials")) {
+                String listSeperator = sample.substring(9, 10);
+                LOG.debug("FOUND SEPARATOR: " + listSeperator);
+                this.listSeparator = listSeperator;
+                this.isAgTrails = true;
+                this.isAgTrailsExist = true;
                 break;
             }
         }
